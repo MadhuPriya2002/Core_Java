435ebc6dcf2950a0e6b993db2d24465ab200bf37
Madhu_Priya <madhupriyajivilikapally@gmail.com>
2023-02-21 14:54:39 +0530

    Commit

--- src/exception/ExceptionHandling.java
+++ src/exception/ExceptionHandling.java
@@ -1,4 +1,23 @@
 package exception;
 
 public class ExceptionHandling {
+
+    public void validateAge(int age){
+        if(age > 18){
+            System.out.println("Exectued");
+        }
+        else {
+            throw new ArithmeticException("Age is not vaild");
+        }
+    }
+
+    public static void main(String[] args) {
+        ExceptionHandling exe = new ExceptionHandling();
+        try{
+            exe.validateAge(21);
+        }
+        catch (ArithmeticException e){
+            System.out.println("Enter Age is not Vaild");
+        }
+    }
 }
